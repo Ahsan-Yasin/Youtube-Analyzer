f6f773f34f502d2d431184f1fd3aeca816214bd8
Ahsan-Yasin <f233063@cfd.nu.edu.pk>
2025-07-27 19:01:10 +0000

    changes

--- src/database.py
+++ src/database.py
@@ -1,16 +1,21 @@
 import chromadb 
-from embedding import MyEmbeddingFunction 
+from src.embedding import MyEmbeddingFunction 
 
 embedding = MyEmbeddingFunction()  
 
-def createDatabase(data: list[str]) -> None:
+def createDatabase(data: list[str],flag=True) -> None:
     client = chromadb.PersistentClient(path="chroma_persistent_storage")
 
     database = client.get_or_create_collection(
         name="YoutubeAnalyzisDatabase",  
         embedding_function=embedding    
     )
-
+    if flag:   #if a new url is passed we will delete the old data but if user says to keep the old data we will keep the whole database 
+        delId=[str(i) for i in range(database.count())] 
+        if delId:
+            database.delete(ids= delId)  
+        else:
+            print("Database is empty")
     idList = [str(i) for i in range(len(data))]  
 
     database.add(ids=idList, documents=data)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
-from load_comments import get_comments  
-from spliter import split  
-from database import createDatabase
+from src.load_comments import get_comments  
+from src.spliter import split  
+from src.database import createDatabase
 
-data= get_comments('https://www.youtube.com/watch?v=i-Cy3SDxLzM&ab_channel=MadHat')
-data= split(data) 
-createDatabase(data)
-print(len(data))
-
-for i in data[:2]:
-    print("\nChunks : ")
-    print (i)
+def UseUrl(url):
+    data= get_comments(url)
+    data= split(data) 
+    createDatabase(data)
+    print(len(data))
+    
+    # for i in data[:2]:
+    #     print("\nChunks : ")
+    #     print (i)
