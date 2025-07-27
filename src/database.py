6ccd8ae16a1ca6aa4cb3619f884ea57ac764e53e
Ahsan-Yasin <f233063@cfd.nu.edu.pk>
2025-07-27 18:32:42 +0000

    creating database

--- src/database.py
+++ src/database.py
@@ -7,12 +7,21 @@ def createDatabase(data: list[str]) -> None:
     client = chromadb.PersistentClient(path="chroma_persistent_storage")
 
     database = client.get_or_create_collection(
-        name="YoutubeAnalyzisDatabase",  # Avoid spaces in collection names
+        name="YoutubeAnalyzisDatabase",  
         embedding_function=embedding    
     )
 
-    idList = [str(i) for i in range(len(data))]  # Generate unique string IDs
+    idList = [str(i) for i in range(len(data))]  
 
     database.add(ids=idList, documents=data)
 
     print(f"Stored {database.count()} documents in ChromaDB.")
+def loadDatabase(): 
+    client = chromadb.PersistentClient(path="chroma_persistent_storage")
+     
+    database = client.get_or_create_collection(
+        name="YoutubeAnalyzisDatabase",  
+        embedding_function=embedding
+    )
+    
+    return database
--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from load_comments import get_comments  
 from spliter import split  
-
+from database import createDatabase
 
 data= get_comments('https://www.youtube.com/watch?v=i-Cy3SDxLzM&ab_channel=MadHat')
-data= split(data)
+data= split(data) 
+createDatabase(data)
 print(len(data))
 
-for i in data[:5]:
+for i in data[:2]:
     print("\nChunks : ")
     print (i)
